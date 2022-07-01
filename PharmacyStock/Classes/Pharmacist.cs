f3e2043f6f2d61a2141365c74a16ac35f61aa68b
Ahmedpop1 <ahmdhsyno@gmail.com>
2022-07-01 03:11:55 +0200

    connected to databbase and created 5 tables

---
+++ PharmacyStock/Classes/Pharmacist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyStock.Classes
+{
+    internal class Pharmacist
+    {
+        [Key]
+        public int ID { get; set; }
+        public int BranchID { get; set; }
+        public string Name { get; set; }
+        public int MobileNumber { get; set; }
+
+        public virtual List<SupplyBill> supplyBills { get; set; }
+       
+    }
+}
---
+++ PharmacyStock/Classes/PharmacyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyStock.Classes
+{
+    internal class PharmacyContext : DbContext
+    {
+        public PharmacyContext() : base("name=DefaultConnection")
+        { }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Pharmacist> pharmacists { get; set; }
+        public DbSet<SupplyBill> supplyBills { get; set; }  
+
+    }
+}
